cb68325602a45077f8882a9424ddeec2f6c3ab70
Himanshu Malviya <76220055+Himanshu664@users.noreply.github.com>
2022-02-03 09:47:40 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    // } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int lcs(String str1,String str2,int m,int n){
+        int[][] t=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                t[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+	public int minOperations(String str1, String str2) 
+	{ 
+	    int m=str1.length();
+	    int n=str2.length();
+	    int noOfDeletion=m-lcs(str1,str2,m,n);
+	    int noOfInsertion=n-lcs(str1,str2,m,n);
+	    return noOfDeletion+noOfInsertion;
+	} 
+}
