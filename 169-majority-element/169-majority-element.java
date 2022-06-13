4e2add4d45b546d296238e179d0c7a0837fc5329
Himanshu Malviya <76220055+Himanshu664@users.noreply.github.com>
2022-06-13 09:06:28 +0530

    Time: 2 ms (83.73%), Space: 56.7 MB (22.54%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,5 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int val=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(val==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count==0){
+                val=nums[i];
+                count=1; 
+            }
+        }
+        int countMaj=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                countMaj++;
+            }
+        }
+        if(countMaj>nums.length/2){
+            return val;
+        }
         
 //         if(nums.length==1){
 //             return nums[0];
@@ -16,7 +38,8 @@ class Solution {
 //                 }
 //         }
 //         return -1;
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+//         Arrays.sort(nums);
+//         return nums[nums.length/2];
+        return -1;
     }
 }
